e3fddc3d85fc3a5455754b86cbc74fef28646de0
Martin Kearn <martin.kearn@microsoft.com>
2021-09-08 10:12:11 +0100

    Ported tutorial

---
+++ GetStartedAzure/MyStack.cs
@@ -0,0 +1,68 @@
+using System.Threading.Tasks;
+using Pulumi;
+using Pulumi.AzureNative.Resources;
+using Pulumi.AzureNative.Storage;
+using Pulumi.AzureNative.Storage.Inputs;
+
+/// <summary>
+/// See https://www.pulumi.com/docs/get-started/azure/
+/// </summary>
+class MyStack : Stack
+{
+    public MyStack()
+    {
+        // Create an Azure Resource Group
+        var resourceGroup = new ResourceGroup("resourceGroup");
+
+        // Create an Azure resource (Storage Account)
+        var storageAccount = new StorageAccount("sa", new StorageAccountArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            Sku = new SkuArgs
+            {
+                Name = SkuName.Standard_LRS
+            },
+            Kind = Kind.StorageV2
+        });
+
+        // Enable static website support
+        var staticWebsite = new StorageAccountStaticWebsite("staticWebsite", new StorageAccountStaticWebsiteArgs
+        {
+            AccountName = storageAccount.Name,
+            ResourceGroupName = resourceGroup.Name,
+            IndexDocument = "index.html",
+        });
+
+        // Upload the file
+        var index_html = new Blob("index.html", new BlobArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            AccountName = storageAccount.Name,
+            ContainerName = staticWebsite.ContainerName,
+            Source = new FileAsset("index.html"),
+            ContentType = "text/html",
+        });
+
+        // Web endpoint to the website
+        this.StaticEndpoint = storageAccount.PrimaryEndpoints.Apply(primaryEndpoints => primaryEndpoints.Web);
+
+        // Export the primary key of the Storage Account
+        this.PrimaryStorageKey = Output.Tuple(resourceGroup.Name, storageAccount.Name).Apply(names => Output.CreateSecret(GetStorageAccountPrimaryKey(names.Item1, names.Item2)));
+    }
+
+    [Output]
+    public Output<string> PrimaryStorageKey { get; set; }
+
+    [Output]
+    public Output<string> StaticEndpoint { get; set; }
+
+    private static async Task<string> GetStorageAccountPrimaryKey(string resourceGroupName, string accountName)
+    {
+        var accountKeys = await ListStorageAccountKeys.InvokeAsync(new ListStorageAccountKeysArgs
+        {
+            ResourceGroupName = resourceGroupName,
+            AccountName = accountName
+        });
+        return accountKeys.Keys[0].Value;
+    }
+}
