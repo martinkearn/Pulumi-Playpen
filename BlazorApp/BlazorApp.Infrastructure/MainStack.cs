e32192569c35ef663d3009507ce1c538db6eef6e
Martin Kearn <martin.kearn@microsoft.com>
2021-09-09 15:14:48 +0100

    Rename to MainStack

--- BlazorApp/BlazorApp.Infrastructure/MainStack.cs
+++ BlazorApp/BlazorApp.Infrastructure/MainStack.cs
@@ -4,9 +4,9 @@ using Pulumi.AzureNative.Resources;
 using Pulumi.AzureNative.Storage;
 using Pulumi.AzureNative.Storage.Inputs;
 
-class MyStack : Stack
+class MainStack : Stack
 {
-    public MyStack()
+    public MainStack()
     {
         // Create an Azure Resource Group
         var resourceGroup = new ResourceGroup("resourceGroup");
