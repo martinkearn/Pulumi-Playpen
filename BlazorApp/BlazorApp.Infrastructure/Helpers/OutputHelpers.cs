2deaf4461d46365269e815e4890b76ab05581d9e
Martin Kearn <martin.kearn@microsoft.com>
2021-09-10 08:34:09 +0100

    Working blazor app

---
+++ BlazorApp/BlazorApp.Infrastructure/Helpers/OutputHelpers.cs
@@ -0,0 +1,36 @@
+﻿using Pulumi;
+using Pulumi.AzureNative.Resources;
+using Pulumi.AzureNative.Storage;
+using System;
+
+namespace BlazorApp.Infrastructure.Helpers
+{
+    public static class OutputHelpers
+    {
+        public static Output<string> SignedBlobReadUrl(Blob blob, BlobContainer container, StorageAccount account, ResourceGroup resourceGroup)
+        {
+            return Output.Tuple(blob.Name, container.Name, account.Name, resourceGroup.Name)
+                .Apply(t =>
+                {
+                    (string blobName, string containerName, string accountName, string resourceGroupName) = t;
+
+                    var blobSAS = ListStorageAccountServiceSAS.InvokeAsync(new ListStorageAccountServiceSASArgs
+                    {
+                        AccountName = accountName,
+                        Protocols = HttpProtocol.Https,
+                        SharedAccessStartTime = DateTime.Now.Subtract(new TimeSpan(365, 0, 0, 0)).ToString("yyyy-MM-dd"),
+                        SharedAccessExpiryTime = DateTime.Now.AddDays(3650).ToString("yyyy-MM-dd"),
+                        Resource = SignedResource.C,
+                        ResourceGroupName = resourceGroupName,
+                        Permissions = Permissions.R,
+                        CanonicalizedResource = "/blob/" + accountName + "/" + containerName,
+                        ContentType = "application/json",
+                        CacheControl = "max-age=5",
+                        ContentDisposition = "inline",
+                        ContentEncoding = "deflate",
+                    });
+                    return Output.Format($"https://{accountName}.blob.core.windows.net/{containerName}/{blobName}?{blobSAS.Result.ServiceSasToken}");
+                });
+        }
+    }
+}
--- BlazorApp/BlazorApp.Infrastructure/MainStack.cs
+++ BlazorApp/BlazorApp.Infrastructure/MainStack.cs
@@ -1,17 +1,21 @@
-using System.Threading.Tasks;
+using BlazorApp.Infrastructure.Helpers;
 using Pulumi;
+using Pulumi.AzureNative.Insights;
 using Pulumi.AzureNative.Resources;
 using Pulumi.AzureNative.Storage;
 using Pulumi.AzureNative.Storage.Inputs;
+using Pulumi.AzureNative.Web;
+using Pulumi.AzureNative.Web.Inputs;
 
 class MainStack : Stack
 {
+    private const string AppName = "BlazorServer";
     public MainStack()
     {
         // Create an Azure Resource Group
         var resourceGroup = new ResourceGroup("BlazorApp");
 
-        // Create an Azure resource (Storage Account)
+        // Create storage account
         var storageAccount = new StorageAccount("storage", new StorageAccountArgs
         {
             ResourceGroupName = resourceGroup.Name,
@@ -19,7 +23,76 @@ class MainStack : Stack
             {
                 Name = SkuName.Standard_LRS
             },
-            Kind = Kind.StorageV2
+            Kind = Pulumi.AzureNative.Storage.Kind.StorageV2
         });
+
+        // Add blob container to storage account
+        var container = new BlobContainer("deploymentzips", new BlobContainerArgs
+        {
+            AccountName = storageAccount.Name,
+            PublicAccess = PublicAccess.None,
+            ResourceGroupName = resourceGroup.Name,
+        });
+
+        // Create a zip of the app's publish output and upload it to the blob container
+        var blob = new Blob($"{AppName.ToLowerInvariant()}.zip", new BlobArgs
+        {
+            AccountName = storageAccount.Name,
+            ContainerName = container.Name,
+            ResourceGroupName = resourceGroup.Name,
+            Type = BlobType.Block,
+            Source = new FileArchive($"..\\{nameof(BlazorApp)}.{AppName}\\bin\\Debug\\net6.0\\publish") // This path should be set to the output of `dotnet publish` command
+        });
+
+        // Generate SAS url for the app output zip in storage
+        var deploymentZipBlobSasUrl = OutputHelpers.SignedBlobReadUrl(blob, container, storageAccount, resourceGroup);
+
+        // Create application insights
+        var appInsights = new Component("appinsights", new ComponentArgs
+        {
+            ApplicationType = ApplicationType.Web,
+            Kind = "web",
+            ResourceGroupName = resourceGroup.Name,
+        });
+
+        // Create app service plan for function app
+        var appServicePlan = new AppServicePlan("appserviceplan", new AppServicePlanArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+
+            // Consumption plan SKU. Make sure you choose valid settings for the type of App Service you are creating.
+            Sku = new SkuDescriptionArgs
+            {
+                Tier = "Standard",
+                Name = "F1"
+            }
+        });
+
+        // Create function app. Set WEBSITE_RUN_FROM_PACKAGE to use the zip in storage
+        var app = new WebApp($"{AppName.ToLowerInvariant()}appservice", new WebAppArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            ServerFarmId = appServicePlan.Id,
+            SiteConfig = new SiteConfigArgs
+            {
+                AppSettings = new[]
+                {
+                    new NameValuePairArgs{
+                        Name = "WEBSITE_RUN_FROM_PACKAGE",
+                        Value = deploymentZipBlobSasUrl,
+                    },
+                    new NameValuePairArgs{
+                        Name = "APPLICATIONINSIGHTS_CONNECTION_STRING",
+                        Value = Output.Format($"InstrumentationKey={appInsights.InstrumentationKey}"),
+                    },
+                },
+            },
+        });
+
+        // Output the app service url
+        this.AppServiceUrl = Output.Format($"https://{app.DefaultHostName}");
     }
+
+    [Output]
+    public Output<string> AppServiceUrl { get; set; }
 }
