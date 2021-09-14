9bb022ebeadd31c52b5aa5bf3f4b109c0a41fec5
Martin Kearn <martin.kearn@microsoft.com>
2021-09-14 16:32:23 +0100

    Update MyStack.cs

--- CSharpFunctionDemo/Infrastructure/MyStack.cs
+++ CSharpFunctionDemo/Infrastructure/MyStack.cs
@@ -3,6 +3,8 @@ using Pulumi;
 using Pulumi.AzureNative.Resources;
 using Pulumi.AzureNative.Storage;
 using Pulumi.AzureNative.Storage.Inputs;
+using Pulumi.AzureNative.Web;
+using Pulumi.AzureNative.Web.Inputs;
 
 class MyStack : Stack
 {
@@ -20,6 +22,66 @@ class MyStack : Stack
                 Name = SkuName.Standard_LRS
             },
             Kind = Kind.StorageV2
+        });
+
+        // Add blob container to storage account
+        var container = new BlobContainer("functionzips", new BlobContainerArgs
+        {
+            AccountName = storageAccount.Name,
+            PublicAccess = PublicAccess.None,
+            ResourceGroupName = resourceGroup.Name,
+        });
+
+        // Create a zip of the function's publish output and upload it to the blob container
+        var blob = new Blob($"function.zip", new BlobArgs
+        {
+            AccountName = storageAccount.Name,
+            ContainerName = container.Name,
+            ResourceGroupName = resourceGroup.Name,
+            Type = BlobType.Block,
+            Source = new FileArchive($"..\\Function\\bin\\Debug\\netcoreapp3.1\\publish")
+        });
+
+        // Create app service plan for function app
+        var appServicePlan = new AppServicePlan("appserviceplan", new AppServicePlanArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            // Consumption plan SKU
+            Sku = new SkuDescriptionArgs
+            {
+                Tier = "Dynamic",
+                Name = "Y1"
+            }
+        });
+
+        // Create function app. Set WEBSITE_RUN_FROM_PACKAGE to use the zip in storage
+        var app = new WebApp($"appservice", new WebAppArgs
+        {
+            Kind = "FunctionApp",
+            ResourceGroupName = resourceGroup.Name,
+            ServerFarmId = appServicePlan.Id,
+            SiteConfig = new SiteConfigArgs
+            {
+                AppSettings = new[]
+                {
+                    new NameValuePairArgs{
+                        Name = "AzureWebJobsStorage",
+                        Value = OutputHelpers.GetConnectionString(resourceGroup.Name, storageAccount.Name),
+                    },
+                    new NameValuePairArgs{
+                        Name = "FUNCTIONS_WORKER_RUNTIME",
+                        Value = "dotnet",
+                    },
+                    new NameValuePairArgs{
+                        Name = "FUNCTIONS_EXTENSION_VERSION",
+                        Value = "~3",
+                    },
+                    new NameValuePairArgs{
+                        Name = "WEBSITE_RUN_FROM_PACKAGE",
+                        Value = OutputHelpers.SignedBlobReadUrl(blob, container, storageAccount, resourceGroup),
+                    },
+                },
+            }
         });
 
         // Export the primary key of the Storage Account
